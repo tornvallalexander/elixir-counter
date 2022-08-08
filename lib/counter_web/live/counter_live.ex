2cd50d7ac52e2851803805cf68384fccdef88d03
Alexander Törnvall <alexander.tornvall@icloud.com>
2022-08-08 10:09:11 +0200

    feat(events): handling counter events

--- lib/counter_web/live/counter_live.ex
+++ lib/counter_web/live/counter_live.ex
@@ -4,13 +4,25 @@ defmodule CounterWeb.CounterLive do
   def render(assigns) do
     ~L"""
     <h1>Count: <%= @count %></h1>
-    <button>+</button>
-    <button>-</button>
+    <button phx-click="increment">+</button>
+    <button phx-click="decrement">-</button>
     """
   end
 
   def mount(_params, _other, socket) do
     socket = assign(socket, :count, 0)
     {:ok, socket}
+  end
+
+  def handle_event("increment", _, socket) do
+    count = socket.assigns.count + 1
+    socket = assign(socket, :count, count)
+    {:noreply, socket}
+  end
+
+  def handle_event("decrement", _, socket) do
+    count = socket.assigns.count - 1
+    socket = assign(socket, :count, count)
+    {:noreply, socket}
   end
 end
